8e31ab1a6fcd7239ab82801fed50bc63a400bd47
Kommodore <Kommodore@users.noreply.github.com>
2018-05-11 21:45:54 +0200

    Added basic rmi functionality for main methods

--- Client/src/ClientProxyImpl.java
+++ Client/src/ClientProxyImpl.java
@@ -1,3 +1,6 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 public class ClientProxyImpl implements ClientProxy{
@@ -8,6 +11,20 @@ public class ClientProxyImpl implements ClientProxy{
 	}
 	
 	public static void main(String[] args) {
-	
+		String serverUrl = "rmi://localhost:1099/ChatServer";
+		
+		try {
+			ChatServer stub = (ChatServer)Naming.lookup(serverUrl);
+			
+		} catch (NotBoundException e) {
+			System.err.println("Remote object not found. Is the server started?");
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			System.err.println("URL not valid. Please check if "+serverUrl+" is a valid URL.");
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			System.err.println("Please check if the server and registry are running.");
+			e.printStackTrace();
+		}
 	}
 }
--- Server/src/ChatServerImpl.java
+++ Server/src/ChatServerImpl.java
@@ -1,6 +1,9 @@
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
-public class ChatServerImpl implements ChatServer{
+public class ChatServerImpl implements ChatServer, ChatProxy{
 	
 	@Override
 	public ChatProxy subscribeUser(String username, ClientProxy handle) throws RemoteException {
@@ -12,7 +15,35 @@ public class ChatServerImpl implements ChatServer{
 		return null;
 	}
 	
-	public static void main(String[] args) {
+	@Override
+	public void sendMessage(String message) throws RemoteException {
 	
+	}
+	
+	public static void main(String[] args) {
+		Registry registry = null;
+		
+		try {
+			registry = LocateRegistry.getRegistry();
+		} catch (RemoteException e) {
+			System.err.println("Remote Exception occurred while getting registry:");
+			e.printStackTrace();
+		}
+		
+		if(registry == null){
+			System.err.println("Cannot find registry. Exiting");
+			System.exit(1);
+		}
+		
+		ChatServerImpl export = new ChatServerImpl();
+		try {
+			ChatServer stub = (ChatServer)UnicastRemoteObject.exportObject(export, 0);
+			registry.rebind("ChatServer", stub);
+			System.out.println("Chat server registered as \"ChatServer\"");
+		} catch (RemoteException e) {
+			System.err.println("Remote Exception occurred while registering the server.");
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 }
